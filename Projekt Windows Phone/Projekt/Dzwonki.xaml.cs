1838d81eb08bb77a22d0b48fef2695399b5f8d42
Wookashlab <lukaszlab1@gmail.com>
2016-04-21 15:54:55 +0200

    Dzwonki (błędny licznik)

--- Projekt Windows Phone/Projekt/Dzwonki.xaml.cs
+++ Projekt Windows Phone/Projekt/Dzwonki.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
@@ -15,6 +16,106 @@ namespace Projekt
         public Dzwonki()
         {
             InitializeComponent();
+            sprawdz();
+            
+
         }
+        void sprawdz()
+        {
+            reset();
+            string czas = DateTime.Now.ToString("HHmm");
+            int dzwonek = 0;
+            int test = Int32.Parse(czas);
+            if (test < 850)
+            {
+                godzina1.Foreground = new SolidColorBrush(Colors.Red);
+                dzwonek = 850;
+            }
+            else if (test < 940)
+            {
+                godzina2.Foreground = new SolidColorBrush(Colors.Red);
+                dzwonek = 940;
+            }
+            else if (test < 1035)
+            {
+                godzina3.Foreground = new SolidColorBrush(Colors.Red);
+                dzwonek = 1035;
+            }
+            else if (test < 1125)
+            {
+                godzina4.Foreground = new SolidColorBrush(Colors.Red);
+                dzwonek = 1125;
+            }
+            else if (test < 1220)
+            {
+                godzina5.Foreground = new SolidColorBrush(Colors.Red);
+                dzwonek = 1220;
+            }
+            else if (test < 1310)
+            {
+                godzina6.Foreground = new SolidColorBrush(Colors.Red);
+                dzwonek = 1310;
+            }
+            else if (test < 1405)
+            {
+                godzina7.Foreground = new SolidColorBrush(Colors.Red);
+                dzwonek = 1405;
+            }
+            else if (test < 1455)
+            {
+                godzina8.Foreground = new SolidColorBrush(Colors.Red);
+                dzwonek = 1455;
+            }
+            else if (test < 1550)
+            {
+                godzina9.Foreground = new SolidColorBrush(Colors.Red);
+                dzwonek = 1550;
+            }
+            else if (test < 1645)
+            {
+                godzina10.Foreground = new SolidColorBrush(Colors.Red);
+                dzwonek = 1645;
+            }
+            else if (test < 1735)
+            {
+                godzina11.Foreground = new SolidColorBrush(Colors.Red);
+                dzwonek = 1735;
+            }
+            else if (test < 1825)
+            {
+                godzina12.Foreground = new SolidColorBrush(Colors.Red);
+                dzwonek = 1825;
+
+            }
+            else 
+            {
+                licznik.Text = "Dziś już więcej zajęć nie masz :) ";
+            }
+            
+            licznik.Text += (dzwonek - test).ToString() + "min";
+
+        }
+        void reset()
+        {
+            licznik.Text = "Do dzwonka zostało ~";
+            godzina1.Foreground = new SolidColorBrush(Colors.White);
+            godzina2.Foreground = new SolidColorBrush(Colors.White);
+            godzina3.Foreground = new SolidColorBrush(Colors.White);
+            godzina4.Foreground = new SolidColorBrush(Colors.White);
+            godzina5.Foreground = new SolidColorBrush(Colors.White);
+            godzina6.Foreground = new SolidColorBrush(Colors.White);
+            godzina7.Foreground = new SolidColorBrush(Colors.White);
+            godzina8.Foreground = new SolidColorBrush(Colors.White);
+            godzina9.Foreground = new SolidColorBrush(Colors.White);
+            godzina10.Foreground = new SolidColorBrush(Colors.White);
+            godzina11.Foreground = new SolidColorBrush(Colors.White);
+            godzina12.Foreground = new SolidColorBrush(Colors.White);
+        }
+
+        private void bRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            sprawdz();
+        }
+
     }
 }
