f795958e5e4e6d7758872f16c985a84cded80fd7
Wookashlab <lukaszlab1@gmail.com>
2016-04-26 11:35:08 +0200

    Ustawienia - dodanie pasku loading i kilku opcji w ustawieniach

--- Projekt Windows Phone/Projekt/Ustawienia.xaml.cs
+++ Projekt Windows Phone/Projekt/Ustawienia.xaml.cs
@@ -9,6 +9,7 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Projekt.Resources;
 using System.IO;
+using System.Windows.Media.Imaging;
 using System.Text.RegularExpressions;
 
 namespace Projekt
@@ -21,6 +22,10 @@ namespace Projekt
         }
         private void Update_Click(object sender, RoutedEventArgs e)
         {
+            SystemTray.ProgressIndicator = new ProgressIndicator();
+            SystemTray.ProgressIndicator.IsVisible = true;
+            SystemTray.ProgressIndicator.IsIndeterminate = true;
+            SystemTray.ProgressIndicator.Text = "Aktualizacja bazy danych";
             var webClient = new WebClient();
             webClient.OpenReadAsync(new Uri("http://hein.bluequeen.tk/aktualizuj.php"));
             webClient.OpenReadCompleted += new OpenReadCompletedEventHandler(aktualizuj);
@@ -31,8 +36,18 @@ namespace Projekt
             using (var reader = new StreamReader(e.Result))
             {
                 textBlock.Text = "Aktualizacja skonczona";
+                SystemTray.ProgressIndicator.IsVisible = false;
             }
         }
+
+       
+
+        private void bOnas_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("Program AMW Helper został stworzny Macieja Hein i Łukasza Labuda. \nStudentów 3 roku informatyki na Akademi Marynarki wojejnnej potrzeby zaliczenia przedmiotu - Aplikacje internetowe i mobile");
+        }
+
+      
 
     }
 }
